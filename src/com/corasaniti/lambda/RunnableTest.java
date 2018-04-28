82c2dda5d0f6e55bd36e8440c3d38de7942f55e3
Piero Corasaniti <corasaniti@gmail.com>
2018-04-28 13:51:23 +0200

    Add Lamda Expression Syntax and Example

--- src/com/corasaniti/lambda/RunnableTest.java
+++ src/com/corasaniti/lambda/RunnableTest.java
@@ -1,5 +1,21 @@
 package com.corasaniti.lambda;
 
+/*
+ * 	Lamda Expression Syntax
+ *  () -> expression;
+ *  () -> {body}
+ *  
+ *  (parameters) -> expression;
+ *  (parameters) -> {body}
+ *  
+ *  Example
+ *  (int x, int y)	->	x + y;
+ *  () -> System.out.println("Hello world with Lambda!");
+ *  (String s) -> { System.out.println(s); }
+ *  (int a, int b, String message) -> { System.out.println("Sum " + a+b); System.out.println(message); }
+ */
+
+
 public class RunnableTest {
 	public static void main(String[] args) {
 		 
