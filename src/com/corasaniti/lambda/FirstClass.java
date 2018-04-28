b33ae508e570caa74c44d85754c2a4ac9f9ac64a
Piero Corasaniti <corasaniti@gmail.com>
2018-04-28 11:23:35 +0200

    Update FirstClass Example

--- src/com/corasaniti/lambda/FirstClass.java
+++ src/com/corasaniti/lambda/FirstClass.java
@@ -2,5 +2,6 @@ package com.corasaniti.lambda;
 
 public class FirstClass {
 	public static void main(String[] args) {
+		System.out.println("Hello this is Lamba Expression Java Project Test");
 	}
 }
